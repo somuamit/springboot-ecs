00cdc27a39403a507ff713909a7f2e00ef88fbc6
Amit Singh <somuamit@gmail.com>
2025-09-20 13:21:21 +0530

    changes

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 	
 	@GetMapping("/greetings")
 	public String sayHi() {
-		return "Welcome to ECS ECR World!";
+		return "Welcome to ECS ECR Testing !";
 	}
 	@GetMapping
 	public String demoapp() {

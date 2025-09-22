4ca5a8849da7993320ad551431ac4743674701d0
somuamit <somuamit@gmail.com>
2025-09-22 10:52:05 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello!";
+		return "Say Hello!";
 	}
 	
 	@GetMapping("/greetings")
